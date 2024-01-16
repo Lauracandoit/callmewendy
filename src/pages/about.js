a3ab070015800494dd0e2c4ed833b216093bba78
Lê Nguyệt <>
2024-01-16 20:29:44 +0700

    Committing my local changes

--- src/pages/about.js
+++ src/pages/about.js
@@ -48,16 +48,16 @@ const about = () => {
       <div className='grid w-full grid-cols-8 gap-16 sm:gap-8  md:grid-cols-8'>
             <div className='col-span-3 flex flex-col items-start justify-start xl:col-span-4 md:order-2 md:!col-span-8 '>
               <h2 className='mb-4 text-lg font-bold uppercase text-dark/75 dark:text-light/75'>Biography </h2>
-              <p className=' text-dark font-medium dark:text-light '> Hello! I&apos;m Wendy, and I have a genuine passion for creating captivating digital experiences. As a seasoned web developer, I specialize in building websites and mobile apps that are not just visually appealing but also intuitive and user-focused.</p>
+              <p className=' text-dark font-medium dark:text-light '> Hello! Im Wendy, and I have a genuine passion for creating captivating digital experiences. As a seasoned web developer, I specialize in building websites and mobile apps that are not just visually appealing but also intuitive and user-focused.</p>
 
               <p className=' text-dark font-medium dark:text-light mt-5 mb-5'>
               Beyond my technical expertise, I also serve as the Customer Support Team Lead at a leading streaming platform. This dual role has provided me with invaluable insights into user behavior, needs, and expectations. Every day, I engage with customers, collaborate with my colleagues, and strategize with my team to ensure we meet and exceed our performance metrics.</p>
              
-              <p className=' text-dark font-medium dark:text-light'> What drives me is the challenge and excitement of bringing clients&apos; visions to life in the digital realm. Whether I&apos;m deep in the code of a new website, refining a mobile app interface, or leading my team towards achieving our customer service goals, I approach each task with dedication and a commitment to excellence. </p>
+              <p className=' text-dark font-medium dark:text-light'> What drives me is the challenge and excitement of bringing clients visions to life in the digital realm. Whether Im deep in the code of a new website, refining a mobile app interface, or leading my team towards achieving our customer service goals, I approach each task with dedication and a commitment to excellence. </p>
             
               <p className=' text-dark font-medium dark:text-light mt-5 mb-5 '> I believe in continuous learning and innovation. This mindset allows me to stay ahead of industry trends and integrate the latest best practices into my work. I'm always on the lookout for opportunities to collaborate with like-minded professionals and organizations. </p>
             
-              <p className=' text-dark font-medium dark:text-light'> If you&apos;re seeking someone who brings a blend of technical prowess, user-centric design, and a proactive leadership approach, I&apos;d love to discuss how I can contribute to your next project </p>            
+              <p className=' text-dark font-medium dark:text-light'> If youre seeking someone who brings a blend of technical prowess, user-centric design, and a proactive leadership approach, Id love to discuss how I can contribute to your next project </p>            
             </div>
 
             <div className='col-span-3 relative h-max rounded-2xl border-2 border-solid border-dark bg-light p-8 dark:bg-dark dark:border-light xl:col-span-4 md:order-1 md:!col-span-8  '>

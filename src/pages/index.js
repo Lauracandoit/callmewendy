374221a350dccf9c12fe40fcdc75795df057fdf1
Lauracandoit <nguyetlt07@gmail.com>
2024-03-01 14:37:18 +0700

    update cv

--- src/pages/index.js
+++ src/pages/index.js
@@ -62,7 +62,7 @@ export default function Home() {
               </p>
               <div className="flex items-center self-start mt-2 lg:self-center ">
                 <Link
-                  href="/lenguyet.pdf"
+                  href="/nguyetle.pdf"
                   target={"_blank"}
                   className="flex items-center bg-dark text-light p-2.5 px-6 rounded-lg text-lg border-white font-semibold hover:bg-white hover:text-black border-solid border-transparent hover:border-blackk border-2 dark:bg-light dark:text-dark hover:dark:bg-dark hover:dark:text-light hover:dark:border-light md:p-2 md:px-4 md:text-base"
                   download={true}

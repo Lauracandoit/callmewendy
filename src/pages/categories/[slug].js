899a24192b518521ca6c80aa66790cdaa7bc65d3
Lauracandoit <nguyetlt07@gmail.com>
2024-03-26 14:40:42 +0700

    update css 2pm 26Mar

--- src/pages/categories/[slug].js
+++ src/pages/categories/[slug].js
@@ -26,14 +26,16 @@ const CategoriesPage = () => {
       <div className="px-32 flex flex-col">
         <h1 className="mt-6 font-semibold text-5xl  "> #{router.query.slug}</h1>
         <span className="mt-2 inline-block">
-          
           Discover more categories and expand your knowlegde
         </span>
       </div>
       <Categories categories={allCategories} currentSlug={router.query.slug} />
-      <div className="grid grid-cols-3 grid-rows-2 gap-16 mt-24 px-32">
+      <div className="grid grid-cols-3 tablet:grid-cols-2 grid-rows-2 gap-16 mt-24 px-32">
         {blogs.map((blog, index) => (
-          <article key={index} className="col-span-1 row-span-1 relative">
+          <article
+            key={index}
+            className="col-span-1 mobile:col-span-3 tablet:col-span-1 sm:col-span-3 row-span-1 relative"
+          >
             <BlogLayoutThree blog={blog} basePath="../" />{" "}
           </article>
         ))}
